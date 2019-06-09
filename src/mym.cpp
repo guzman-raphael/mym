4e41f18446a13d5dbad4c14a08db1111c7bc89c2
guzman-raphael <raphael.h.guzman@gmail.com>
2019-06-09 16:09:44 -0500

    Add ssl options.

--- src/mym.cpp
+++ src/mym.cpp
@@ -448,31 +448,10 @@ void mexFunction(int nlhs, mxArray*plhs[], int nrhs, const mxArray*prhs[]) {
             mexErrMsgTxt("Couldn\'t initialize MySQL connection object");
         //my_bool  my_true = true;
         // mysql_options(conn, MYSQL_OPT_RECONNECT, &my_true);
-        // char*ssl_input = "none";
         // char*default_cipher = "DHE-RSA-AES256-SHA:AES256-SHA:AES128-SHA";
         // int*selection = (int *)2;
         // // enum op { SSL_MODE_REQUIRED, SSL_MODE_DISABLED } selection;
         // // selection = SSL_MODE_REQUIRED;
-
-        // if (nrhs>=(jarg+5))
-        //     ssl_input = getstring(prhs[jarg+4]);
-
-        // if (!strcasecmp(ssl_input, "true")) {
-        //     ssl_input = "true";
-        //     mysql_options(conn, MYSQL_OPT_SSL_CIPHER, default_cipher);
-        //     mysql_options(conn, MYSQL_OPT_SSL_MODE, selection);
-        //     // mysql_options(conn, MYSQL_OPT_SSL_MODE, default_cipher);
-        //     // mysql_options(conn, MYSQL_OPT_SSL_MODE, *SSL_MODE_REQUIRED);
-        //     // mexPrintf("Hello Raphael\n");
-        // }
-        // else if (!strcasecmp(ssl_input, "false")) {
-        //     ssl_input = "false";
-        // }
-        // else {
-        //     ssl_input = "none";
-        //     mysql_options(conn, MYSQL_OPT_SSL_CIPHER, default_cipher); //Raphael MYSQL_OPT_SSL_CAPATH, "/etc/ssl/certs"
-        // }
-
         // apt install cmake libssl-dev libncurses5-dev pkg-config
         // mexPrintf("Hello Raphael\n");
         // mexPrintf("this is ssl: %s\n",ssl_input);
@@ -480,6 +459,29 @@ void mexFunction(int nlhs, mxArray*plhs[], int nrhs, const mxArray*prhs[]) {
         //     q = OPEN;
         // else if (!strcasecmp(query, "close"))
         //     q = CLOSE;
+        // SSL_MODE_DISABLED = 1,
+        // SSL_MODE_PREFERRED,
+        // SSL_MODE_REQUIRED,
+        // enum use_this { SSL_MODE_DISABLED, SSL_MODE_PREFERRED, SSL_MODE_REQUIRED } selection;
+        // selection = SSL_MODE_REQUIRED;
+        // mysql_options(conn, MYSQL_OPT_SSL_MODE, "SSL_MODE_REQUIRED");
+        
+        char*ssl_input = "none";
+        int mode_option = SSL_MODE_PREFERRED;
+
+        if (nrhs>=(jarg+5))
+            ssl_input = getstring(prhs[jarg+4]);
+
+        if (!strcasecmp(ssl_input, "true")) {
+            ssl_input = "true";
+            mode_option = SSL_MODE_REQUIRED;
+        }
+        else if (!strcasecmp(ssl_input, "false")) {
+            ssl_input = "false";
+            mode_option = SSL_MODE_DISABLED;
+        }
+
+        mysql_options(conn, MYSQL_OPT_SSL_MODE, &mode_option);
 
         if (nlhs<1) {
             mexPrintf("Connecting to  host = %s", (host) ? host : "localhost");
@@ -489,7 +491,7 @@ void mexFunction(int nlhs, mxArray*plhs[], int nrhs, const mxArray*prhs[]) {
                 mexPrintf("  user = %s", user);
             if (pass)
                 mexPrintf("  password = %s", pass);
-            // mexPrintf("  ssl = %s", ssl_input);
+            mexPrintf("  ssl = %s", ssl_input);
             mexPrintf("\n");
         }
 
@@ -592,7 +594,6 @@ void mexFunction(int nlhs, mxArray*plhs[], int nrhs, const mxArray*prhs[]) {
                     mexPrintf("%d connections open\n", nconn);
                 if (nconn==0) {
                     mexPrintf("No connections open\n");
-                    mexPrintf("This is Raphael-5.3\n");
                     return;
                 }
                 if ((nconn==1) && (c[0].isopen)) {
